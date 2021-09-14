83cc37aa37d880cb97fd0e73ab524d428a97257d
TI\franklin.escobar <Franklin.Escobar_Ramos@telus.com>
2021-09-13 19:52:53 -0600

    Practica

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import reactDom from "react-dom";
+
+import FormularioFormik from "./components/FormularioFormik/FormularioFormik";
+
+reactDom.render(<FormularioFormik />, document.getElementById("root"));
